7c41f88a2c385e06fe3eea8de37a668d06812f4c
Edblas <gbs.adilio@gmail.com>
2026-01-05 22:30:01 -0300

    feat: add Simples Nacional link to SideMenu

--- AssistBPO-Web/frontend/src/components/layout/SideMenu.jsx
+++ AssistBPO-Web/frontend/src/components/layout/SideMenu.jsx
@@ -4,6 +4,14 @@ export function SideMenu({ setView, toggleDarkMode, dark }) {
   return (
     <div className="fixed top-20 right-4 w-44 bg-white dark:bg-gray-800 border rounded-md p-3 space-y-2 shadow z-40">
       <a
+        href="https://www8.receita.fazenda.gov.br/simplesnacional/aplicacoes.aspx?id=21"
+        target="_blank"
+        rel="noreferrer"
+        className="block text-sm px-2 py-1 rounded bg-blue-50 dark:bg-gray-700"
+      >
+        Simples Nacional
+      </a>
+      <a
         href="https://grupometa-my.sharepoint.com/:o:/r/personal/mauricio_konig_meta_com_br/_layouts/15/Doc.aspx?sourcedoc=%7Bc25ac0fa-50eb-4c80-a819-1cd9d982f327%7D&action=view&wd=target(RENDA%20PJ.one%7C12361420-7a37-41e0-950d-97ea5c687924%2FSimples%20Nacional%7Ca5dbd916-a336-4e5f-a5ca-d4bc7bdb2fb9%2F)&wdorigin=NavigationUrl"
         target="_blank"
         rel="noreferrer"
